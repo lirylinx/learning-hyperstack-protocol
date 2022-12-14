51e6251c6734d4b6b29f4ebefe8a6234bccc1a31
lirylinx <dercioguirruta44@gmail.com>
2022-12-14 11:51:38 +0200

    p2p posting & voting

---
+++ p2p-autobase/hypernews.js
@@ -0,0 +1,146 @@
+import minimist from 'minimist';
+import Corestore from 'corestore';
+import Hyperswarm from 'hyperswarm';
+import Autobase from 'autobase';
+import crypto from 'crypto';
+import lexint from 'lexicographic-integer';
+import ram from 'random-access-memory';
+
+
+const args = minimist(process.argv, {
+    alias: {
+        writers: 'w',
+        indexes: 'i',
+        storage: 's',
+        name: 'n'
+    },
+    default: {
+        swarm: true
+    }, 
+    boolean: ['ram', 'swarm']
+});
+
+class Hypernews {
+    constructor () {
+        this.store = new Corestore(args.ram ? ram : ( args.storage || 'hypernews'));
+        this.swarm = null;
+        this.autobase = null;
+        this.bee = null;
+        this.name = null;
+    }
+
+
+    async start() {
+        const writer = this.store.get({name: 'writer'});
+        const autobaseIndex = this.store.get({name: 'index'});
+
+        await writer.ready();
+
+        this.name = args.name || writer.key.slice(0, 8).toString('hex');
+        this.autobase = new Autobase([writer], { indexes: autobaseIndex });
+
+        for ( const w of [].concat(args.writers || [] )) {
+            await this.autobase.addInput(this.store.get(Buffer.from(w, 'hex')));
+        }
+
+        for ( const i of [].concat(args.indexes || [])) {
+            await this.autobase.addDefaultIndex(this.store.get(Buffer.from(i, 'hex')));
+        }
+
+        await this.autobase.ready();
+
+        if ( args.swarm) {
+            const topico = Buffer.from(sha256(this.name), 'hex');
+            this.swarm = new Hyperswarm();
+            this.swarm.on('connection', (socket) => this.store.replicate(socket));
+            this.swarm.join(topico);
+            await this.swarm.flush();
+            process.once('SIGINT', () => this.swarm.destroy()); // Para reinicio rapido
+        }
+
+        this.info();
+
+        const self = this;
+        const view = this.autobase.linearize({
+            unwrap: true,
+            async apply (batch) {
+                const b = self.bee.batch({ update: false });
+
+                for ( const { value } of batch ) {
+                    const op = JSON.parse(value);
+
+                    if ( op.type === 'post') {
+                        const hash = sha256(op.data);
+                        await b.put('posts!' + hash, { hash, votes: 0, data: op.data});
+                    }
+                }
+                await b.flush();
+            }
+        });
+
+        this.bee = new Hyperbee(view, {
+            extension: false,
+            keyEncoding: 'utf-8',
+            valueEncoding: 'json'
+        });
+    }
+
+    info() {
+        console.log("Configuração Autobase.");
+        console.log();
+        console.log('hrepl hypernews.js ' + 
+            '-n ' + this.name + ' ' + 
+            this.autobase.inputs.map( i => '-w ' + i.key.toString('hex')).join(' ') + ' ' + 
+            this.autobase.defaultOutputs.map( i => '-i ' + i.key.toString('hex')).join(' ')
+        );
+        console.log();
+        console.log('Para usar outro directorio usar o comando --storage ./another');
+        console.log('Para desabilitar swarm --no-swarm');
+        console.log();
+    }
+
+
+    async * all() {
+        for await ( const data of this.bee.createReadStream({ gt: 'posts!', lt: 'posts!~'})) {
+            yield data.value;
+        }
+    }
+
+
+    // Metodos para postagem
+    async post ( text ) {
+        const hash = sha256(text);
+
+        await this.autobase.append(JSON.stringify({
+            type: 'post',
+            hash,
+            data: text
+        }));
+    }
+
+    // metodo para Voto de postagem
+    async upvote( hash ) {
+        await this.autobase.append(JSON.stringify({
+            type: 'vote',
+            hash,
+            up: true
+        }));
+    }
+
+    //Metodo para remover voto de uma postagem
+    async downvote( hash ) {
+        await this.autobase.append(JSON.stringify({
+            type: 'vote',
+            hash,
+            up: false
+        }));
+    }
+}
+
+
+export const hypernews = new Hypernews();
+await hypernews.start();
+
+function sha256 (inp) { 
+    return crypto.createHash('sha256').update(inp).digest('hex');
+}
